e736cff789fee2647f48a2a1a4ffed175183ff60
Nathan Hopkins <natehop@gmail.com>
2019-10-21 21:53:16 -0600

    Updates to snake

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -4,27 +4,6 @@ class DemosController < ApplicationController
   before_action :verify, only: [:show]
   before_action :setup_demo, only: [:show]
 
-  def show
-    case params[:id]
-    when "snake"
-      unless @stimulus_reflex
-        session[:direction] = "right"
-        session[:clock] = false
-        session[:speed] = 0.2
-        session[:grid_x] = 30
-        session[:grid_y] = 30
-        session[:length] = 4
-        session[:snake] = []
-        session[:start_x] = 50
-        session[:start_y] = 50
-        session[:food] = []
-        session[:alive] = true
-      end
-      @grid_x = session[:grid_x] * 10 + 1
-      @grid_y = session[:grid_y] * 10 + 1
-    end
-  end
-
   private
 
   def verify
@@ -40,5 +19,23 @@ class DemosController < ApplicationController
   def setup_chat
     session[:chat_color] ||= "red"
     @chats ||= Rails.cache.read(:chats) || []
+  end
+
+  def setup_snake
+    unless @stimulus_reflex
+      session[:direction] = "right"
+      session[:clock] = false
+      session[:speed] = 0.1
+      session[:grid_x] = 30
+      session[:grid_y] = 30
+      session[:length] = 4
+      session[:snake] = []
+      session[:start_x] = 50
+      session[:start_y] = 50
+      session[:food] = []
+      session[:alive] = true
+    end
+    @grid_x = session[:grid_x] * 10 + 1
+    @grid_y = session[:grid_y] * 10 + 1
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  ActionCable.server.config.logger = Logger.new(nil)
 end
