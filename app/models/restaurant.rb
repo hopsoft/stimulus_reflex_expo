5feb7fe798c4f917242a6fcbabb3c956a8cd3f1a
pjf <pjforde1978@gmail.com>
2019-10-28 20:00:28 -0400

    technically don't need model validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -16,9 +16,5 @@
 class Restaurant < ApplicationRecord
   scope :filtered, ->(name) { where("lower(name) LIKE ?", "%#{name&.downcase}%") }
   scope :sorted, ->(column, direction = :forward) { order(Arel.sql("#{column}#{" DESC" if direction == :reverse}")) }
-
-  validates_inclusion_of :stars, in: 0..5
-  validates_inclusion_of :price, in: 1..3
-
   FILTERS = %w[name stars price category].freeze
 end
