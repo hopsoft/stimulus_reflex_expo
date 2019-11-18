996995b0976c5b36ffcb57c6020fcdfd36442a3f
pjf <pjforde1978@gmail.com>
2019-11-18 11:01:40 -0500

    release tasks

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sed '8s/^...//' app/javascript/controllers/bucket_lists_controller.js
-wget -O InertiaPlugin.js $INERTIA_PLUGIN
-wget -O VelocityTracker.js $VELOCITY_TRACKER
+sed '8s/^...//' app/javascript/controllers/bucket_lists_controller.js > tmp/bucket_lists_controller.js
+mv tmp/bucket_lists_controller.js app/javascript/controllers/bucket_lists_controller.js
+wget -O node_modules/gsap/InertiaPlugin.js $INERTIA_PLUGIN
+wget -O node_modules/gsap/VelocityTracker.js $VELOCITY_TRACKER
 bundle exec rails db:migrate
