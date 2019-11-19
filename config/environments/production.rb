31abd6077048d1a38eae57b47d6014d2096d9c20
pjf <pjforde1978@gmail.com>
2019-11-18 20:39:51 -0500

    fixed paths

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  `/app/release-tasks.sh`
+  `./release-tasks.sh`
 
   # Code is not reloaded between requests.
   config.cache_classes = true
--- release-tasks.sh
+++ release-tasks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed '8s/^...//' /app/app/javascript/controllers/bucket_lists_controller.js > /app/tmp/bucket_lists_controller.js
-mv /app/tmp/bucket_lists_controller.js /app/app/javascript/controllers/bucket_lists_controller.js
-wget -O /app/node_modules/gsap/InertiaPlugin.js $INERTIA_PLUGIN
-wget -O /app/node_modules/gsap/VelocityTracker.js $VELOCITY_TRACKER
+sed '8s/^...//' app/javascript/controllers/bucket_lists_controller.js > tmp/bucket_lists_controller.js
+mv tmp/bucket_lists_controller.js app/javascript/controllers/bucket_lists_controller.js
+wget -O node_modules/gsap/InertiaPlugin.js $INERTIA_PLUGIN
+wget -O node_modules/gsap/VelocityTracker.js $VELOCITY_TRACKER
