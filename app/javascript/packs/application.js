c80e8c2067fe9f91bbff0581e2636db430e0532c
pjf <pjforde1978@gmail.com>
2019-12-15 19:53:50 -0500

    convert pack to imports

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require('@rails/ujs').start()
-require('turbolinks').start()
-require('@rails/activestorage').start()
-require('channels')
+import Rails from '@rails/ujs'
+Rails.start()
+
+import Turbolinks from 'turbolinks'
+Turbolinks.start()
+
+import * as ActiveStorage from '@rails/activestorage'
+ActiveStorage.start()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -28,4 +32,5 @@ import '@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff'
 import '@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2'
 import 'stylesheets/application.scss'
 import 'lib/focus'
+import 'channels'
 import 'controllers'
