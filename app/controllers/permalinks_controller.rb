9b2098ece13dc9478645babce537ceac7172bd12
Julian Rubisch <julian@julianrubisch.at>
2020-04-12 13:05:51 +0200

    Switch between stimulus_reflex and non_SR requests

--- app/controllers/permalinks_controller.rb
+++ app/controllers/permalinks_controller.rb
@@ -3,9 +3,9 @@
 class PermalinksController < ApplicationController
   def show
     @categories = Restaurant.select(:category).distinct.order(:category).map(&:category)
-    @category = session[:category] || params[:category]
-    @rating = session[:rating] || params[:rating]
-    @price = session[:price] || params[:price]&.split(",")
+    @category ||= @stimulus_reflex ? session[:category] : params[:category] 
+    @rating ||= @stimulus_reflex ? session[:rating] : params[:rating]
+    @price ||= @stimulus_reflex ? session[:price] : params[:price]&.split(",")
 
     @restaurants = Restaurant.all
     @restaurants = @restaurants.where(category: @category) if @category.present?
