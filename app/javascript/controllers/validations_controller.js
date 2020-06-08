f35d3619c44934b3ce31d6916bac9ae9f5c10bf5
Roland Studer <roland.studer@gmail.com>
2020-06-08 15:52:03 +0200

    uses auto serialization of form

--- app/javascript/controllers/validations_controller.js
+++ app/javascript/controllers/validations_controller.js
@@ -3,15 +3,13 @@ import { debounce } from 'lodash-es'
 import ApplicationController from './application_controller'
 
 export default class extends ApplicationController {
-  static targets = ['firstName', 'lastName', 'email', 'submit']
-
   connect () {
     super.connect()
     this.perform = debounce(this._perform, 250)
   }
 
   _perform (event) {
-    this.stimulate('ValidationsReflex#perform', this.userParams)
+    this.stimulate('ValidationsReflex#perform')
   }
 
   reset (event) {
@@ -21,15 +19,5 @@ export default class extends ApplicationController {
 
   validate (event) {
     this.perform()
-  }
-
-  get userParams () {
-    return {
-      user: {
-        first_name: this.firstNameTarget.value,
-        last_name: this.lastNameTarget.value,
-        email: this.emailTarget.value
-      }
-    }
   }
 }
