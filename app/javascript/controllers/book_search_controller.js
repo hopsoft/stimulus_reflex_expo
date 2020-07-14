0de6a4d4674bd61b3781e0143533985a0867c18f
Roland Studer <roland.studer@gmail.com>
2020-07-15 00:45:31 +0200

    use hopsoft/debounced for book search

--- app/javascript/controllers/book_search_controller.js
+++ app/javascript/controllers/book_search_controller.js
@@ -1,20 +1,14 @@
-import { debounce } from 'lodash-es'
 import ApplicationController from './application_controller'
 
 export default class extends ApplicationController {
   static targets = ['query', 'activity', 'count', 'list']
-
-  connect () {
-    super.connect()
-    this.perform = debounce(this._perform, 150).bind(this)
-  }
 
   beforePerform (element, reflex) {
     this.activityTarget.hidden = false
     this.countTarget.hidden = true
   }
 
-  _perform (event) {
+  perform (event) {
     event.preventDefault()
     this.stimulate('BookSearchReflex#perform', this.queryTarget.value)
   }
