3523c6028291181ea78e045856ee2cbbcfe6a6b3
Roland Studer <roland.studer@gmail.com>
2020-07-20 23:40:04 +0200

    Debounced (#51)
    * use hopsoft/debounce
    
    * use hopsoft/debounced for book search
    
    Co-authored-by: Nate Hopkins <natehop@gmail.com>

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,8 +3,10 @@ import { definitionsFromContext } from 'stimulus/webpack-helpers'
 import StimulusReflex from 'stimulus_reflex'
 import consumer from '../channels/consumer'
 import controller from './application_controller'
+import debounced from 'debounced'
 
 const application = Application.start()
 const context = require.context('controllers', true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
 StimulusReflex.initialize(application, { consumer, controller, debug: true })
+debounced.initialize()
--- app/javascript/controllers/validations_controller.js
+++ app/javascript/controllers/validations_controller.js
@@ -1,23 +1,9 @@
 import Turbolinks from 'turbolinks'
-import { debounce } from 'lodash-es'
 import ApplicationController from './application_controller'
 
 export default class extends ApplicationController {
-  connect () {
-    super.connect()
-    this.perform = debounce(this._perform, 250)
-  }
-
-  _perform (event) {
-    this.stimulate('ValidationsReflex#perform')
-  }
-
   reset (event) {
     event.preventDefault()
     Turbolinks.visit(location.href)
-  }
-
-  validate (event) {
-    this.perform()
   }
 }
