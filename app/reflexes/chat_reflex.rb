d08c42a931d28ed44d977a43384dd1eb719c3aab
Nate Hopkins <natehop@gmail.com>
2020-11-29 10:56:58 -0700

    Update to SR 3.4.0 release (#62)
    * Update to SR 3.4.0 release
    
    * More chat refactoring
    
    * Move focus selector to user broadcast
    
    * Use variable for selector
    
    * Better structure
    
    * Update to latest SR pre release
    
    * Update to latest CableReady

--- app/reflexes/chat_reflex.rb
+++ app/reflexes/chat_reflex.rb
@@ -1,21 +1,38 @@
 # frozen_string_literal: true
 
 class ChatReflex < ApplicationReflex
-  include CableReady::Broadcaster
+  include ActionView::Helpers::SanitizeHelper
 
-  def post(color, message, message_id)
-    @chats = Rails.cache.read(:chats) || []
-    @chats.shift while @chats.size > 1000
-    @chats << {
-      id: message_id,
+  def post(color, message)
+    morph :nothing
+
+    message = strip_tags(message[0, 100])
+    return if message.blank?
+
+    chat = {
       color: color,
       author: request.remote_ip,
       message: message,
       created_at: Time.current.iso8601
     }
-    Rails.cache.write :chats, @chats
-    cable_ready["chat"].dispatch_event name: "chats:added", detail: {message_id: message_id}
-    cable_ready.broadcast
+
+    chat_html = ChatsController.render(partial: "chats/chat", locals: {chat: chat, color: color})
+    list_selector = "##{color} [data-target='chat.list']"
+    input_selector = "##{color} textarea"
+
+    # broadcast to the user that triggered the reflex
+    cable_ready.set_value(selector: input_selector, value: "", focus_selector: input_selector).broadcast
+
+    # global broadcast to everyone
+    cable_ready["chat"]
+      .insert_adjacent_html(selector: list_selector, html: chat_html)
+      .dispatch_event(name: "chats:added")
+      .broadcast
+
+    # persist data
+    chats = Rails.cache.read(:chats) || []
+    chats << chat
+    Rails.cache.write :chats, chats
   end
 
   def set_color
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   config.action_controller.enable_fragment_cache_logging = true
 
-  config.cache_store = :redis_cache_store, {url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" }}
+  config.cache_store = :redis_cache_store, {url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1")}
   config.session_store :cache_store, key: "_stimulus_reflex_expo_development_session"
   config.public_file_server.headers = {
     "Cache-Control" => "public, max-age=#{2.days.to_i}"
