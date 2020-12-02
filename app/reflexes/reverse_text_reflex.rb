fbf4c500dd28660ed0ea13c1e18ddd250df8e5f0
Hopsoft <natehop@gmail.com>
2020-12-02 07:59:13 -0700

    Pull reverse text up onto a single line

--- app/reflexes/reverse_text_reflex.rb
+++ app/reflexes/reverse_text_reflex.rb
@@ -3,8 +3,6 @@
 class ReverseTextReflex < ApplicationReflex
   def perform
     morph :nothing
-    cable_ready
-      .text_content(selector: "#reversed-text", text: element[:value].reverse)
-      .broadcast
+    cable_ready.text_content(selector: "#reversed-text", text: element[:value].reverse).broadcast
   end
 end
